c9523502614d177cce5cb43afedfebc009aa2c15
Harrison He <harrisonhe1@gmail.com>
2020-03-20 22:58:53 -0400

    Restart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 
 import React from 'react'
-import {
-    AppBar,
-    Toolbar,
-    Typography
-} from '@material-ui/core'
-import Home from './Pages/Home'
 
 const App = () => (
-    <>
-        <AppBar>
-            <Toolbar>
-                <Typography variant="h6">
-                    PostsReaction
-            </Typography>
-            </Toolbar>
-        </AppBar>
-        <Home />
-    </>
+    <h1>App</h1>
 )
 
 export default App
