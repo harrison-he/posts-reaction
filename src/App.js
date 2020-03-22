41962216fd9d4208bbfe000baa191430192b7a63
Harrison He <harrisonhe1@gmail.com>
2020-03-22 00:32:15 -0400

    Use react router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
 
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardActions,
-    Button
-} from '@material-ui/core'
-import {
-    Comment
-} from '@material-ui/icons'
-
-const url = "https://jsonplaceholder.typicode.com"
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom'
+import Feed from './Feed'
 
 const App = () => {
-    const [users, setUsers] = useState([])
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        (async () => {
-            try {
-                setUsers((await axios.get(`${url}/users`)).data)
-                setPosts((await axios.get(`${url}/posts`)).data)
-            } catch (err) {
-                console.log(err)
-            }
-        })()
-    }, [])
-
+    
     return (
-        <>
-            {posts.map(({ id, userId, title, body }) => {
-                const { name, username } = users.find(({ id }) => id === userId) || {}
-
-                return (
-                    <Card key={id}>
-                        <CardHeader
-                            title={name}
-                            subheader={username}
-                        />
-                        <CardContent>
-                            {title}
-                            {body}
-                        </CardContent>
-                        <CardActions>
-                            <Button>
-                                <Comment />Comments
-                            </Button>
-                        </CardActions>
-                    </Card>
-                )
-            })}
-        </>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Feed />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
---
+++ src/Feed.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardActions,
+    Button
+} from '@material-ui/core'
+import {
+    InsertCommentOutlined
+} from '@material-ui/icons'
+
+const url = "https://jsonplaceholder.typicode.com"
+
+const Feed = () => {
+    const [users, setUsers] = useState([])
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                setUsers((await axios.get(`${url}/users`)).data)
+                setPosts((await axios.get(`${url}/posts`)).data)
+            } catch (err) {
+                console.log(err)
+            }
+        })()
+    }, [])
+
+
+    return (
+        <>
+            {posts.map(({ id, userId, title, body }) => {
+                const { name, username } = users.find(({ id }) => id === userId) || {}
+
+                return (
+                    <Card key={id}>
+                        <CardHeader
+                            title={name}
+                            subheader={username}
+                        />
+                        <CardContent>
+                            {title}
+                            {body}
+                        </CardContent>
+                        <CardActions>
+                            <Button>
+                                <InsertCommentOutlined fontSize="small" />Comments
+                        </Button>
+                        </CardActions>
+                    </Card>
+                )
+            })}
+        </>
+    )
+}
+
+export default Feed
